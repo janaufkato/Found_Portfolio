65bc7b008d048c636b823217e0c3a233fe3dfcc8
unknown <www.janaufkato@gmail.com>
2018-06-16 20:14:47 +0300

    leng_blockPart1/4

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $('#input0').click(function() {
   if($("select#input0 :selected").val() == "eng") {
-    $("select#input0").attr('style', 'background-image:url(..);');
+    $("select#input0").attr('style', ';');
+
  }
  if($("select#input0 :selected").val() == "rus") {
-    $("select#input0").attr('style', 'background-image:url(..);');
+    $('#home').text('главная');
+    $('#works').text('работы');
+    $('#contact').text('контакты');
  }
     console.log('select color: '+$("select#input0 :selected").val());
 });
